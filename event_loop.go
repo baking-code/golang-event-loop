3ad9c676f9e34d4dc9380eddd95a2ffd52ad878b
Benjamin King <benjamin.allan.king@gmail.com>
2023-11-14 13:35:37 +0000

    Add try catch behaviour

--- event_loop.go
+++ event_loop.go
@@ -2,12 +2,13 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Hello, World!")
-}
-
+/*
+Execute array of asynchronous functions and return array in order of response.
+Mimics behaviour of Javascript's Event loop
+*/
 func EventLoop[T interface{}](inputFunctions []func() T) []T {
 	var toReturn []T
+	var errors []error
 
 	mainChannel := make(chan T, 1)
 	done := make(chan bool)
@@ -17,7 +18,7 @@ func EventLoop[T interface{}](inputFunctions []func() T) []T {
 			value, isMore := <-mainChannel
 			if isMore {
 				toReturn = append(toReturn, value)
-				if len(toReturn) == cap(inputFunctions) {
+				if len(toReturn)+len(errors) == cap(inputFunctions) {
 					close(mainChannel)
 				}
 			} else {
@@ -28,13 +29,23 @@ func EventLoop[T interface{}](inputFunctions []func() T) []T {
 		}
 	}()
 
-	for _, inputFunction := range inputFunctions {
+	for index, inputFunction := range inputFunctions {
 		executor := inputFunction
+		i := index
 		go func() {
+			defer func() {
+				if r := recover(); r != nil {
+					// catch any unhandled errors, print and continue execution. Like wrapping in a try/catch
+					errors = append(errors, fmt.Errorf("Received error from input function %d: %s", i, r))
+				}
+			}()
 			mainChannel <- executor()
 		}()
 	}
 	// wait for done channel to receive signal
 	<-done
+	if len(errors) > 0 {
+		fmt.Printf("%+v\n", errors)
+	}
 	return toReturn
 }
--- event_loop_test.go
+++ event_loop_test.go
@@ -6,29 +6,51 @@ import (
 	"time"
 )
 
-func TestLoopOne(t *testing.T) {
+func TestLoopOrder(t *testing.T) {
 	var input = []func() string{
-		func() string { time.Sleep(1 * time.Second); return "a" },
-		func() string { time.Sleep(3 * time.Second); return "b" },
-		func() string { time.Sleep(2 * time.Second); return "c" },
+		func() string { time.Sleep(3 * time.Second); return "a" },
+		func() string { time.Sleep(2 * time.Second); return "b" },
+		func() string { time.Sleep(5 * time.Second); return "c" },
+		func() string { time.Sleep(1 * time.Second); return "d" },
+		func() string { time.Sleep(4 * time.Second); return "e" },
 	}
 
 	got := EventLoop[string](input)
-	var expected = []string{"a", "c", "b"}
+	var expected = []string{"d", "b", "a", "e", "c"}
 	if !reflect.DeepEqual(expected, got) {
 		t.Errorf("EventLoop(); no good, got %s", got)
 	}
 }
 
-func TestLoopTwo(t *testing.T) {
+func TestLoopParallel(t *testing.T) {
+	timeStart := time.Now()
 	var input = []func() string{
-		func() string { time.Sleep(2 * time.Second); return "a" },
+		func() string { time.Sleep(4 * time.Second); return "a" },
 		func() string { time.Sleep(1 * time.Second); return "b" },
-		func() string { time.Sleep(3 * time.Second); return "c" },
+		func() string { time.Sleep(5 * time.Second); return "c" },
 	}
 
 	got := EventLoop[string](input)
+	elapsed := time.Since((timeStart))
 	var expected = []string{"b", "a", "c"}
+	if !reflect.DeepEqual(expected, got) {
+		t.Errorf("EventLoop(); no good, got %s", got)
+	}
+	// verify that we're running in parallel
+	if elapsed.Seconds() > 7 {
+		t.Errorf("EventLoop(); should have been run in parallel. Expected: between 5 and 6s; Actual: %d", elapsed)
+	}
+}
+
+func TestLoopErrorHandling(t *testing.T) {
+	var input = []func() string{
+		func() string { time.Sleep(3 * time.Second); return "a" },
+		func() string { time.Sleep(1 * time.Second); return "b" },
+		func() string { time.Sleep(2 * time.Second); panic("help!") },
+	}
+
+	got := EventLoop[string](input)
+	var expected = []string{"b", "a"}
 	if !reflect.DeepEqual(expected, got) {
 		t.Errorf("EventLoop(); no good, got %s", got)
 	}
