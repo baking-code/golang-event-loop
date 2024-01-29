d8bdb49a087ab741e9ad51be32928a52870badb3
Benjamin King <benjamin.allan.king@gmail.com>
2024-01-29 19:44:15 +0000

    Refactor channel use to user wg

--- event_loop/event_loop.go
+++ event_loop/event_loop.go
@@ -1,6 +1,9 @@
 package eventloop
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 /*
 Execute array of asynchronous functions and return array in order of response.
@@ -10,40 +13,24 @@ func EventLoop[T interface{}](inputFunctions []func() T) []T {
 	var toReturn []T
 	var errors []error
 
-	mainChannel := make(chan T, 1)
-	done := make(chan bool)
-
-	go func() {
-		for {
-			value, isMore := <-mainChannel
-			if isMore {
-				toReturn = append(toReturn, value)
-				if len(toReturn)+len(errors) == cap(inputFunctions) {
-					close(mainChannel)
-				}
-			} else {
-				// mainChannel closed, trigger done channel
-				done <- true
-				return
-			}
-		}
-	}()
-
+	var wg sync.WaitGroup
+	wg.Add(len(inputFunctions))
 	for index, inputFunction := range inputFunctions {
 		executor := inputFunction
 		i := index
 		go func() {
+			defer wg.Done()
 			defer func() {
 				if r := recover(); r != nil {
 					// catch any unhandled errors, print and continue execution. Like wrapping in a try/catch
-					errors = append(errors, fmt.Errorf("Received error from input function %d: %s", i, r))
+					errors = append(errors, fmt.Errorf("received error from input function %d: %s", i, r))
 				}
 			}()
-			mainChannel <- executor()
+			toReturn = append(toReturn, executor())
 		}()
 	}
 	// wait for done channel to receive signal
-	<-done
+	wg.Wait()
 	if len(errors) > 0 {
 		fmt.Printf("%+v\n", errors)
 	}
--- promise_all/promise_all.go
+++ promise_all/promise_all.go
@@ -1,46 +1,25 @@
 package promise
 
+import "sync"
+
 /*
 Implements javascripts Promise.all by evaluating array of async
 operations in parallel, but returning their values in their given
 order
 */
 func PromiseAll[T interface{}](inputFunctions []func() T) []T {
-	var toReturn = make([]T, len(inputFunctions), len(inputFunctions))
-	type ValueIndexTuple struct {
-		value T
-		index int
-	}
-	mainChannel := make(chan ValueIndexTuple, 1)
-	done := make(chan bool)
+	var toReturn = make([]T, len(inputFunctions))
 
-	go func() {
-		times := 0
-		for {
-			tuple, isMore := <-mainChannel
-			if isMore {
-
-				toReturn[tuple.index] = tuple.value
-				times++
-				if times == cap(inputFunctions) {
-					close(mainChannel)
-				}
-			} else {
-				// mainChannel closed, trigger done channel
-				done <- true
-				return
-			}
-		}
-	}()
-
+	var wg sync.WaitGroup
+	wg.Add(len(inputFunctions))
 	for index, inputFunction := range inputFunctions {
 		executor := inputFunction
 		i := index
 		go func() {
-			mainChannel <- ValueIndexTuple{value: executor(), index: i}
+			defer wg.Done()
+			toReturn[i] = executor()
 		}()
 	}
-	// wait for done channel to receive signal
-	<-done
+	wg.Wait()
 	return toReturn
 }
